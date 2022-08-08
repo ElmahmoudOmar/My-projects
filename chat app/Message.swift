69ec2b24b193baa1823e2ea1cb98b7a26fccd183
ElmahmoudOmar <105306409+ElmahmoudOmar@users.noreply.github.com>
2022-08-08 11:25:23 +0100

    Add files via upload

---
+++ chat app/Message.swift
@@ -0,0 +1,13 @@
+
+
+import Foundation
+
+
+
+
+struct Message {
+    
+    let sender:String
+    let body:String
+    
+}
---
+++ chat app/Views/MessageCell.swift
@@ -0,0 +1,25 @@
+
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightAvatar: UIImageView!
+    @IBOutlet weak var leftAvatar: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height/5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
